9227f516ac05bf454c9b63669747a3d0b1bae6a9
Ricky <rickytsang.dev@gmail.com>
2025-02-24 00:30:00 +0100

    Create first pages directory

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Hello and welcome to my site!</h1>
+<a href="/about">About my site</a>
---
+++ front/src/routes/signup/+page.svelte
@@ -0,0 +1,5 @@
+<h1>This is signup page </h1>
+<p>please signup to go...</p>
+
+<p>ALready have an accout ? </p>
+<a href="/login">Got to login</a>
