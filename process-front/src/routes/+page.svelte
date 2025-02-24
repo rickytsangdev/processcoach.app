7ce6b733d3cfd481faaa1ee54d2dc8a5209e4641
Ricky <rickytsang.dev@gmail.com>
2025-02-24 01:14:21 +0100

    change frontend project folder to handle tailwindcss and daisyui

---
+++ process-front/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1 class="text-3xl font-bold underline text-red-500">Hello and welcome to my site!</h1>
+<a href="/about">About my site</a>
+
+
+<button class="btn">Hello daisyUI</button>
---
+++ process-front/src/routes/login/+page.svelte
@@ -0,0 +1,5 @@
+<h1>This is login page </h1>
+<p>please login to access .</p>
+
+<p>No account ? </p>
+<a href="/signup">Got to Signup</a>
